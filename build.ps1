f4752eeb345bd810bca05592b764614a90d0d4d6
Alex2021cyberhuman <alex2021cyberhuman@gmail.com>
2022-01-19 23:45:05 +0500

    feat: Update configurations

---
+++ build.ps1
@@ -0,0 +1,3 @@
+﻿./src/articles-microservice/prebuild.Dockerfile.ps1 -PathToRepository ./src/articles-microservice/
+./src/auth-microservice/prebuild.Dockerfile.ps1 -PathToRepository ./src/auth-microservice/
+./src/person-microservice/prebuild.Dockerfile.ps1 -PathToRepository ./src/person-microservice/
--- clean.ps1
+++ clean.ps1
@@ -1,13 +1,18 @@
-﻿$volumes =  @(
+﻿docker compose -f docker-compose.base.yaml -f docker-compose.dev.yaml -f docker-compose.local.yaml rm -s -f --all -v
+
+$volumes = @(
 "volumes/auth-storage/mongo/data"
 "volumes/auth-storage/mongo/logs"
+"volumes/articles-storage/postgres/data"
+"volumes/articles-storage/postgres/logs"
 "volumes/person-storage/neo4j/data"
 "volumes/person-storage/neo4j/logs"
+"src/articles-microservice/app/publish"
+"src/auth-microservice/app/publish"
+"src/person-microservice/app/publish"
 )
-rm $volumes -Force -Recurse
-ni $volumes -ItemType Directory
+Remove-Item $volumes -Force -Recurse
+New-Item $volumes -ItemType Directory
 
-ls -Include bin,obj -Recurse | foreach ($_) { rm $_.FullName -Force -Recurse; echo "removed $($_.FullName)" }
-
-iex "docker compose -f docker-compose.base.yaml -f docker-compose.dev.yaml -f docker-compose.local.yaml rm -f --all"
-
+dotnet clean
+dotnet restore
