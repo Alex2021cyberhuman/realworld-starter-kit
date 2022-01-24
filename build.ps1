cbccb9de0ebb3bdf64213a7416c1f1bacdd41f33
Alex2021cyberhuman <alex2021cyberhuman@gmail.com>
2022-01-24 18:54:02 +0500

    feat: Clean solution

--- build.ps1
+++ build.ps1
@@ -1,16 +1,24 @@
 ﻿param(
-[Boolean]$Restart = $true,
-[Boolean]$NoBuild = $false
+    [Boolean]$RestartMyServices = $true,
+    [Boolean]$NoBuild = $false,
+    [Boolean]$RestartAllServices = $false
 )
 
-if ($NoBuild -eq $false) 
+if ($NoBuild -eq $false)
 {
     ./src/articles-microservice/prebuild.Dockerfile.ps1 -PathToRepository ./src/articles-microservice/
     ./src/auth-microservice/prebuild.Dockerfile.ps1 -PathToRepository ./src/auth-microservice/
-    ./src/person-microservice/prebuild.Dockerfile.ps1 -PathToRepository ./src/person-microservice/    
+    ./src/person-microservice/prebuild.Dockerfile.ps1 -PathToRepository ./src/person-microservice/
 }
 
-if ($Restart) 
+if ($RestartAllServices)
+{
+    docker compose  -f docker-compose.base.yaml -f docker-compose.dev.yaml -f docker-compose.local.yaml stop
+    docker compose  -f docker-compose.base.yaml -f docker-compose.dev.yaml -f docker-compose.local.yaml create
+    docker compose  -f docker-compose.base.yaml -f docker-compose.dev.yaml -f docker-compose.local.yaml restart
+}
+
+if ($RestartMyServices -and -not$RestartAllServices)
 {
     docker compose  -f docker-compose.base.yaml -f docker-compose.dev.yaml -f docker-compose.local.yaml stop articles auth person
     docker compose  -f docker-compose.base.yaml -f docker-compose.dev.yaml -f docker-compose.local.yaml create --force-recreate --build articles auth person
