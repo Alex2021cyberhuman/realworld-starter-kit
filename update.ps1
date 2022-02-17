10122e01cb6187ee01d8a2ea2c03d1f520bcd22f
Alex2021cyberhuman <alex2021cyberhuman@gmail.com>
2022-02-17 22:48:14 +0500

    feat: Update shared-core

---
+++ update.ps1
@@ -0,0 +1,30 @@
+﻿Invoke-Expression -Command "git pull"
+Set-Location -Path src
+Set-Location -Path articles-microservice
+Invoke-Expression -Command "git pull"
+Set-Location -Path shared-core
+Invoke-Expression -Command "git pull"
+Set-Location -Path ../
+Set-Location -Path ../
+
+Set-Location -Path auth-microservice
+Invoke-Expression -Command "git pull"
+Set-Location -Path shared-core
+Invoke-Expression -Command "git pull"
+Set-Location -Path ../
+Set-Location -Path ../
+
+Set-Location -Path likes-microservice
+Invoke-Expression -Command "git pull"
+Set-Location -Path shared-core
+Invoke-Expression -Command "git pull"
+Set-Location -Path ../
+Set-Location -Path ../
+
+Set-Location -Path person-microservice
+Invoke-Expression -Command "git pull"
+Set-Location -Path shared-core
+Invoke-Expression -Command "git pull"
+Set-Location -Path ../
+Set-Location -Path ../
+Set-Location -Path ../
