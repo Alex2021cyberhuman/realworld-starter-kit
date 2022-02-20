a9fb18e8bebcd892e5412f320ee32aaf5164cfc1
Alex2021cyberhuman <alex2021cyberhuman@gmail.com>
2022-02-20 16:05:03 +0500

    feat: Update configuration

--- build.ps1
+++ build.ps1
@@ -9,18 +9,41 @@ if ($NoBuild -eq $false)
     ./src/articles-microservice/prebuild.Dockerfile.ps1 -PathToRepository ./src/articles-microservice/
     ./src/auth-microservice/prebuild.Dockerfile.ps1 -PathToRepository ./src/auth-microservice/
     ./src/person-microservice/prebuild.Dockerfile.ps1 -PathToRepository ./src/person-microservice/
+    ./src/likes-microservice/prebuild.Dockerfile.ps1 -PathToRepository ./src/likes-microservice/
+    ./src/comments-microservice/prebuild.Dockerfile.ps1 -PathToRepository ./src/comments-microservice/
 }
 
 if ($RestartAllServices)
 {
-    docker compose  -f docker-compose.base.yaml -f docker-compose.dev.yaml -f docker-compose.local.yaml stop
-    docker compose  -f docker-compose.base.yaml -f docker-compose.dev.yaml -f docker-compose.local.yaml create
-    docker compose  -f docker-compose.base.yaml -f docker-compose.dev.yaml -f docker-compose.local.yaml restart
+    docker compose `
+        -f docker-compose.base.yaml `
+        -f docker-compose.dev.yaml `
+        -f docker-compose.local.yaml stop
+    docker compose `
+        -f docker-compose.base.yaml `
+        -f docker-compose.dev.yaml `
+        -f docker-compose.local.yaml create
+    docker compose `
+        -f docker-compose.base.yaml `
+        -f docker-compose.dev.yaml `
+        -f docker-compose.local.yaml restart
 }
 
 if ($RestartMyServices -and -not$RestartAllServices)
 {
-    docker compose  -f docker-compose.base.yaml -f docker-compose.dev.yaml -f docker-compose.local.yaml stop articles auth person
-    docker compose  -f docker-compose.base.yaml -f docker-compose.dev.yaml -f docker-compose.local.yaml create --force-recreate --build articles auth person
-    docker compose  -f docker-compose.base.yaml -f docker-compose.dev.yaml -f docker-compose.local.yaml restart articles auth person proxy
+    docker compose `
+        -f docker-compose.base.yaml `
+        -f docker-compose.dev.yaml `
+        -f docker-compose.local.yaml `
+        stop articles auth person comments likes
+    docker compose `
+        -f docker-compose.base.yaml `
+        -f docker-compose.dev.yaml `
+        -f docker-compose.local.yaml `
+        create --force-recreate --build articles auth person comments likes
+    docker compose `
+        -f docker-compose.base.yaml `
+        -f docker-compose.dev.yaml `
+        -f docker-compose.local.yaml `
+        restart articles auth person proxy comments likes
 }
--- clean.ps1
+++ clean.ps1
@@ -1,15 +1,17 @@
 ﻿docker compose -f docker-compose.base.yaml -f docker-compose.dev.yaml -f docker-compose.local.yaml rm -s -f --all -v
 
 $volumes = @(
-"volumes/auth-storage/mongo/data"
-"volumes/auth-storage/mongo/logs"
-"volumes/articles-storage/postgres/data"
-"volumes/articles-storage/postgres/logs"
-"volumes/person-storage/neo4j/data"
-"volumes/person-storage/neo4j/logs"
-"src/articles-microservice/app/publish"
-"src/auth-microservice/app/publish"
-"src/person-microservice/app/publish"
+"./volumes/auth-storage/mongo/data"
+"./volumes/auth-storage/mongo/logs"
+"./volumes/person-storage/neo4j/data"
+"./volumes/person-storage/neo4j/logs"
+"./volumes/articles-storage/postgres/data"
+"./volumes/articles-storage/postgres/logs"
+"./volumes/comments/web-api/logs"
+"./volumes/comments-storage/postgres/data"
+"./volumes/comments-storage/postgres/logs"
+"./volumes/likes-storage/redis/data"
+"./volumes/likes-storage/redis/logs"
 )
 Remove-Item $volumes -Force -Recurse
 New-Item $volumes -ItemType Directory
